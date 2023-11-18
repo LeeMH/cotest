8214fb710f57b77b4c9885d62ca2b2e02b87088d
mhlee <jigul.sw@gmail.com>
2023-11-19 06:52:16 +0900

    1260 풀이 완료

--- 백준온라인/1260.py
+++ 백준온라인/1260.py
@@ -5,14 +5,19 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def dfs(start_node):
-    mydeque = deque()
-    mydeque.append(start_node)
+def solve_dfs(graph, start_node):
+    # 작은 값들부터 방문하기 위해서 역정렬한다.
+    # 그래야 작은값이 stack의 최상위로 들어간다.
+    for k in graph:
+        nodes = graph[k] 
+        nodes.sort(reverse = True)
+
+    stack = [start_node]
     answer = []
     visited = set()
 
-    while mydeque:
-        v = mydeque.pop()
+    while stack:
+        v = stack.pop()
 
         if v not in visited:
             visited.add(v)
@@ -21,36 +26,38 @@ def dfs(start_node):
         if v not in graph:
             continue
         else:
-            for ii in reversed(range(len(graph[v]))):
-                mydeque.append(graph[v][ii])
+            for vv in graph[v]:
+                if vv not in visited:
+                    stack.append(vv)
 
-    print(' '.join(map(str, answer)), end = '\n')
+    return ' '.join(map(str, answer))
 
+def solve_bfs(graph, start_node):
+    # bfs는 queue 방식으로 동작하기 때문에, 오름차순으로 정렬해서 넣는다.
+    for k in graph:
+        nodes = graph[k] 
+        nodes.sort()
 
-def bfs(start_node):
-    mydeque = deque()
-    mydeque.append(start_node)
+    queue = deque()
+    queue.append(start_node)
     answer = []
     visited = set()
 
-    while mydeque:
-        print(mydeque)
-        v = mydeque.popleft()
+    while queue:
+        v = queue.popleft()
 
         if v not in visited:
             answer.append(v)
-            visited.add(v)        
+            visited.add(v)
 
         if v not in graph:
             continue
 
-        print(graph[v])
-
         for k in graph[v]:
             if k not in visited:
-                mydeque.append(k)
+                queue.append(k)
 
-    print(' '.join(map(str, answer)), end = '\n')
+    return ' '.join(map(str, answer))
 
 
 graph = {}
@@ -58,11 +65,17 @@ graph = {}
 N, M, V = map(int, input().split())
 for _ in range(M):
     NUMS = list(map(int, input().split()))
+
     if NUMS[0] in graph:
         graph[NUMS[0]].append(NUMS[1])        
     else:
         graph[NUMS[0]] = [NUMS[1]]
 
-dfs(V)
-bfs(V)
+    if NUMS[1] in graph:
+        graph[NUMS[1]].append(NUMS[0])
+    else:
+        graph[NUMS[1]] = [NUMS[0]]
+
+print(solve_dfs(graph, V), end = '\n')
+print(solve_bfs(graph, V), end = '\n')
 
